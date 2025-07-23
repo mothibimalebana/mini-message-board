87fa6ab66549356b13eceb1c7b8502e5257c4bc3
mothibimalebana <mothibi.mm12@gmail.com>
2025-07-23 21:39:20 +0200

    import Path CommonJs

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const path = require("node:path");
+
+const app = express()
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date()
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date()
+  }
+];
