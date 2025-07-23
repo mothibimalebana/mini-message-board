24502a1a5b2abfc69b082d2275425a84047c9c4b
mothibimalebana <mothibi.mm12@gmail.com>
2025-07-23 23:31:23 +0200

    chore:set up basic express app
    the app displays array of messages provided in app.js

--- app.js
+++ app.js
@@ -2,11 +2,20 @@ const express = require('express')
 const path = require("node:path");
 
 const app = express()
+const assetsPath = path.join(__dirname, "public");
+
+app.use(express.static(assetsPath));
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
+app.get("/", (req, res) => {
+    res.render("index",{ messages })
+});
 
+app.listen(3000, () => {
+    console.log("running")
+})
 const messages = [
   {
     text: "Hi there!",
